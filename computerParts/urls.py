9d396798ad4a7db497e6062116f11366d280b7d9
kni9ht <yogeshcjadhaav@gmail.com>
2022-04-14 14:30:49 +0530

    Heroku Deployment

--- computerParts/urls.py
+++ computerParts/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import web
 from login import urls
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +15,7 @@ urlpatterns = [
     path('desktop/', web.desktop),
     path('accessory/', web.accessory),
     path('laptop/', web.laptop),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
